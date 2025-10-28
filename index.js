1e36cf72a720aa83917a14e3360136d8676470f7
muhammadrayhanwidayat <murayat99@gmail.com>
2025-10-28 14:32:45 +0700

    membuat printilan app

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const app = express();
 const db = require('./models');
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+db.sequelize.sync().then(() => {
+  app.listen(3000, () => {
+    console.log('Server Started');
+  });
+})
+.catch(err => {
+  console.log(err);
+});
